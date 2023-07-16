b7a37cf7eeb9a44fa9ad4c23bb7971145aef7777
MajesticWhale <139717724+MajesticWhale@users.noreply.github.com>
2023-07-16 18:21:00 -0500

    Add files via upload

---
+++ IvanTorres_FirstProject.java
@@ -0,0 +1,16 @@
+		/*-------------
+		 * COSC 1173 Programming Lab
+		 * Name: Ivan Torres
+		 * Data: 1/28/2023
+		 * Display personal information
+		 ----------* */
+public class IvanTorres_FirstProject
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Personal Information");
+		System.out.println("Name: Ivan Torres");
+		System.out.println("Major: Computer Science");
+		System.out.println("Hobbies: Video Games, Puzzle Solving, Casual Programming, Music");
+	}
+}
